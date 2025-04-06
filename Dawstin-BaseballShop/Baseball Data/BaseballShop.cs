76859b9a56f209200ad94b8c86fd4e33c6a21d31
Dawstin Waters <dawstinw4@gmail.com>
2025-04-06 06:31:16 -0700

    Created project

---
+++ Dawstin-BaseballShop/Baseball Data/BaseballShop.cs
@@ -0,0 +1,27 @@
+﻿using Dawstin_BaseballShop.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dawstin_BaseballShop.Baseball_Data
+{
+    public class BaseballShopContext : DbContext
+    {
+        public BaseballShopContext(DbContextOptions<BaseballShopContext> options) : base(options) { }
+
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Event> Events { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Item>()
+                .HasOne(i => i.Category)
+                .WithMany(c => c.Items)
+                .HasForeignKey(i => i.CategoryID);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+}
